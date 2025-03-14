78cf772a9967bd3dbeb97d3a3c8eb6614418f901
Pranay Reddy <82954865+pranayreddy9896@users.noreply.github.com>
2025-03-14 19:59:29 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css';
 import profile_img from '../../assets/profile_img.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import resumePdf from '../../assets/resume.pdf';
 const Hero = () => {
   return (
     <div id='home'className='hero'>
@@ -13,7 +14,8 @@ const Hero = () => {
             crafting intuitive user interfaces or designing robust databases, I thrive on turning ideas into reality.</p>
     <div className="hero-action">
         <div className="hero-connect"> <AnchorLink className='anchor-link' offset={50} href='#contact'> Connect With Me </AnchorLink></div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-resume"><a href={resumePdf} download="Pranay_Resume.pdf">Download Resume</a>
+            </div>
         </div>
     </div>
   )
